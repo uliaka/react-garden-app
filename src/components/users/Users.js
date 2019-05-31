713ea879650e2030229ad29af95e118e416021f1
uliaka <uliana.gorin95@gmail.com>
2019-05-31 20:45:35 +0300

    Sorting by name

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import axios from 'axios';
+import './Users.css';
+import usersData from'./UsersData'
 
 class User extends React.Component {
   render() {
-    return <div> {this.props.data.name} </div>
+    return (
+      <div>
+          <div class="user_block">
+            <h1>{this.props.data.name}</h1>
+            <span class="age"> {this.props.data.age} years old</span>
+            <span class="role">{this.props.data.role}</span>
+         </div>
+      </div>
+    )
   }
+}
+
+function SortButton(props) {
+  return (
+    <button onClick={props.sortUsers}>Sort by name</button>
+  )
 }
 
 function UserList(props) {
   return (
     <div>
       <h1> Users list </h1>
+      <SortButton sortUsers={props.sortUsers}/>
      {props.users.map(item => <User data={item}/>)}
     </div>
   )
@@ -20,16 +37,28 @@ class UserGenery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: [{ name: 'Vasyl', role: 'student', group: '4', age: '20'},
-        { name: 'Oleg', role: 'student', group: '6', age: '21' },
-        { name: 'Nadia', role: 'student', group: '1', age: '19' }]
+      users: usersData,
     }
+  }
+
+  sortUsers() {
+    const users = this.state.users;
+    const sortedUsers = users.sort(function(a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      return 0;
+    })
+    this.setState({ users: sortedUsers })
   }
 
   render() {
     return (
       <div>
-        <UserList users={this.state.users}/>
+        <UserList users={this.state.users} sortUsers={this.sortUsers.bind(this)}/>
       </div>
     )
   }
---
+++ src/components/users/UsersData.js
@@ -0,0 +1,14 @@
+export default [
+  { name: 'Vasyl', role: 'student', group: '4', age: '20'},
+  { name: 'Oleg', role: 'student', group: '6', age: '21' },
+  { name: 'Nadia', role: 'student', group: '1', age: '19' },
+  { name: 'Petro', role: 'sofware', group: '5', age: '19'},
+  { name: 'Julia', role: 'student', group: '4', age: '20'},
+  { name: 'Uliana', role: 'student', group: '6', age: '21' },
+  { name: 'Mysha', role: 'student', group: '1', age: '19' },
+  { name: 'Katia', role: 'sofware', group: '4', age: '20'},
+  { name: 'Margo', role: 'student', group: '4', age: '30'},
+  { name: 'Vova', role: 'student', group: '6', age: '23' },
+  { name: 'Gala', role: 'sofware', group: '1', age: '19' },
+  { name: 'Hihi', role: 'student', group: '4', age: '21'},
+]
