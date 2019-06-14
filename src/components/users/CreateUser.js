39f1d0e3b52a8691c1b365fbf46d00561e8afb4a
uliaka <uliana.gorin95@gmail.com>
2019-06-14 20:29:50 +0300

    Error handeling

--- src/components/users/CreateUser.js
+++ src/components/users/CreateUser.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { UserContext } from '../../UserProvider.js'
 import { UserProvider } from '../../UserProvider'
+import './Users.css';
 
 class CreateUser extends React.Component {
   constructor(props) {
@@ -11,7 +12,12 @@ class CreateUser extends React.Component {
         role: '',
         group: '',
         age: ''
-      }
+      },
+      nameErorr: '',
+      roleErorr: '',
+      groupErorr: '',
+      ageErorr: '',
+      err: ''
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -23,6 +29,25 @@ class CreateUser extends React.Component {
     this.setState({ newUser });
   }
 
+  validate(users) {
+  const { name, role, group, age } = this.state.newUser;
+   let nameErorr = name ? '' : 'name is empty';
+   let roleErorr = role ? '' : 'role is empty';
+   let groupErorr = group ? '' : 'group is empty';
+   let ageErorr = age ? '' : 'age is empty';
+   let err = '';
+   const valUser = users.find(user => name === user.name)
+   if (valUser) {
+     err = 'user exist!!'
+   }
+
+   if (nameErorr || roleErorr || groupErorr || ageErorr || err) {
+     this.setState({ nameErorr, roleErorr, groupErorr, ageErorr, err })
+     return false;
+   }
+   return true;
+  }
+
   resetForm() {
     this.setState({
       newUser: {
@@ -30,7 +55,12 @@ class CreateUser extends React.Component {
         role: '',
         group: '',
         age: ''
-      }
+      },
+      nameErorr: '',
+      roleErorr: '',
+      groupErorr: '',
+      ageErorr: '',
+      err: ''
     })
   }
 
@@ -38,43 +68,52 @@ class CreateUser extends React.Component {
     return (
       <UserContext.Consumer>
         {({users, addUser}) => (
-          <div>
-            <label>
-              Username
-              <input type="text"
-                name="name"
-                value={this.state.newUser.name}
-                onChange={(e) => {this.handleChange('name', e.target.value)}}
-              />
-            </label>
-            <label>
-              Role
-              <input type="text"
-                name="role"
-                value={this.state.newUser.role}
-                onChange={(e) => {this.handleChange('role', e.target.value)}}
-              />
-            </label>
-            <label>
-              Group
-              <input type="text"
-                name="group"
-                value={this.state.newUser.group}
-                onChange={(e) => {this.handleChange('group', e.target.value)}}
-              />
-            </label>
-            <label>
-              Age
-              <input type="text"
-                name="age"
-                value={this.state.newUser.age}
-                onChange={(e) => {this.handleChange('age', e.target.value)}}
-              />
-            </label>
+          <div className="form-create-user">
+            <h2>Add user</h2>
+              <label>
+                Username
+                <input type="text"
+                  name="name"
+                  value={this.state.newUser.name}
+                  onChange={(e) => {this.handleChange('name', e.target.value)}}
+                />
+                <div className = "style-error">{this.state.nameErorr}</div>
+              </label>
+              <label>
+                Role
+                <input type="text"
+                  name="role"
+                  value={this.state.newUser.role}
+                  onChange={(e) => {this.handleChange('role', e.target.value)}}
+                />
+                <div className="style-error">{this.state.roleErorr}</div>
+              </label>
+              <label>
+                Group
+                <input type="text"
+                  name="group"
+                  value={this.state.newUser.group}
+                  onChange={(e) => {this.handleChange('group', e.target.value)}}
+                />
+                <div className="style-error">{this.state.groupErorr}</div>
+              </label>
+              <label>
+                Age
+                <input type="text"
+                  name="age"
+                  value={this.state.newUser.age}
+                  onChange={(e) => {this.handleChange('age', e.target.value)}}
+                />
+                <div className="style-error">{this.state.ageErorr}</div>
+              </label>
+              <div className="style-error">{this.state.err}</div>
             <button
               onClick={() => {
-                addUser(this.state.newUser)
-                this.resetForm()
+                const isValid = this.validate(users);
+                  if (isValid) {
+                  addUser(this.state.newUser)
+                  this.resetForm()
+                }
               }}
             >
               Add user
